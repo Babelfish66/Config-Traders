e9078faa6fac7230120843799c5007fdc6aba47c
Babelfish66 <adkbabelfish@gmail.com>
2015-07-26 13:03:12 +0100

    Create tids.sql

---
+++ tids.sql
@@ -0,0 +1,5 @@
+INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (693, 'Packaged Foods', 900);
+INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (694, 'Drinks', 900);
+INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (695, 'Coocked Meats', 900);
+INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (696, 'Clothes', 900);
+INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (697, 'Backpacks', 900);
