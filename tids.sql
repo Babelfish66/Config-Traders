3979bb50f5e5a0ed79653e179ac95feb0a3faa5c
Babelfish66 <adkbabelfish@gmail.com>
2015-07-30 03:02:56 +0100

    Delete tids.sql

--- tids.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (693, 'Packaged Foods', 900);
-INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (694, 'Drinks', 900);
-INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (695, 'Coocked Meats', 900);
-INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (696, 'Clothes', 900);
-INSERT INTO `trader_tids` (`id`, `name`, `trader`) VALUES (697, 'Backpacks', 900);
